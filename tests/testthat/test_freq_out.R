ff836824e8087d38275d7b006ea0ec4bf122f2b1
Mollie McDowell <molliejmcdowell@gmail.com>
2015-11-19 22:54:34 -0800

    final?

--- tests/testthat/test_freq_out.R
+++ tests/testthat/test_freq_out.R
@@ -4,3 +4,14 @@ test_that("output is data.frame", {
   expect_is(res, "data.frame")
 })
 
+test_that("length of output is greater than zero", {
+  x <- iris$Species
+  res <- dplyr::data_frame(x)
+  expect_gt(length(res), 0)
+})
+
+test_that("output is tbl_df", {
+  x <- iris$Species
+  res <- dplyr::data_frame(x)
+  expect_is(res, "tbl_df")
+})
