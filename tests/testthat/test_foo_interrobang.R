71b779cd4f7db77e15f347a938e8dade67e27c02
Mollie McDowell <molliejmcdowell@gmail.com>
2015-11-19 21:14:41 -0800

    c

--- tests/testthat/test_foo_interrobang.R
+++ tests/testthat/test_foo_interrobang.R
@@ -3,3 +3,15 @@ test_that("output contains interrobang", {
   res <- foo_interrobang(d)
   expect_output(res, "?!", all = FALSE)
 })
+
+test_that("length of output is greater than 1", {
+  d <- "OMG"
+  res <- foo_interrobang(d)
+  expect_gt(length(res), 1)
+})
+
+test_that("class of output is character", {
+  d <- "OMG"
+  res <- foo_interrobang(d)
+  expect_is(res, "character")
+})
